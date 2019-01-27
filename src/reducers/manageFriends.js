9bcad18583bfb14067d8b5f634810d0acb21ea1b
Andrew Jones <joneseyit@protonmail.com>
2019-01-26 19:22:55 -0500

    Think I got it

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,14 @@
 export function manageFriends(state, action){
 }
+
+const manageFrinds = (state, action) => {
+  switch (action.type) {
+    case 'ADD_FRIEND':
+      return { friends: [...state.friends, action.friend]}
+    case 'REMOVE_FREIND':
+        return {friends: (state.friends.filter(curFriend => curFriend !== action.friend))}
+    default:
+      return state
+
+  }
+}
